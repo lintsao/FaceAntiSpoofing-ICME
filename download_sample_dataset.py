97e4fad70a913b0dd2af5754767ada0abd56a76c
homerun-beauty <ethentsao1998@gmail.com>
2022-02-07 16:37:12 +0800

    modified download_sample_dataset.py

--- download_sample_dataset.py
+++ download_sample_dataset.py
@@ -1,5 +1,6 @@
 import gdown
 from zipfile import ZipFile
+import os
 
 url = "https://drive.google.com/u/1/uc?id=1DmVZpIBcYKy87V19poEwviFtmpRJ2ZAH&export=download"
 output = "pr_depth_map_256.zip"
@@ -8,3 +9,5 @@ gdown.download(url, output)
 zf = ZipFile(output, 'r')
 zf.extractall("./")
 zf.close()
+
+os.remove(output)
